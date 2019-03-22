0b186b4ebbfe2895e1a58143694a60ab6a873c92
Cristi Stoicescu <stoicescucristi93@gmail.com>
2019-03-22 16:56:20 +0200

    Update to use babel 7

--- dist/index.js
+++ dist/index.js
@@ -1,33 +1,27 @@
-'use strict';
+export default function JsxDisplayIf(_ref) {
+  var t = _ref.types;
+  return {
+    visitor: {
+      JSXElement: function transform(path) {
+        var node = path.node;
+        var ifAttributes = node.openingElement.attributes.filter(function (_ref2) {
+          var type = _ref2.type,
+              name = _ref2.name;
+          return type === 'JSXAttribute' && name.name === 'display-if';
+        });
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-exports.default = JsxDisplayIf;
-function JsxDisplayIf(_ref) {
-    var t = _ref.types;
-
-    return {
-        visitor: {
-            JSXElement: function transform(path) {
-                var node = path.node;
-
-                var ifAttributes = node.openingElement.attributes.filter(function (_ref2) {
-                    var type = _ref2.type;
-                    var name = _ref2.name;
-                    return type === 'JSXAttribute' && name.name === 'display-if';
-                });
-                if (!ifAttributes.length) {
-                    return;
-                }
-                var ifAttribute = ifAttributes[0];
-                var newJsxOpeningElement = t.JSXOpeningElement(node.openingElement.name, node.openingElement.attributes ? node.openingElement.attributes.filter(function (attr) {
-                    return attr !== ifAttribute;
-                }) : null);
-                var newJsxElement = t.JSXElement(newJsxOpeningElement, node.closingElement, node.children);
-                var conditionalExpression = t.conditionalExpression(ifAttribute.value.expression, newJsxElement, t.nullLiteral());
-                path.replaceWith(conditionalExpression);
-            }
+        if (!ifAttributes.length) {
+          return;
         }
-    };
+
+        var ifAttribute = ifAttributes[0];
+        var newJsxOpeningElement = t.JSXOpeningElement(node.openingElement.name, node.openingElement.attributes ? node.openingElement.attributes.filter(function (attr) {
+          return attr !== ifAttribute;
+        }) : null);
+        var newJsxElement = t.JSXElement(newJsxOpeningElement, node.closingElement, node.children);
+        var conditionalExpression = t.conditionalExpression(ifAttribute.value.expression, newJsxElement, t.nullLiteral());
+        path.replaceWith(conditionalExpression);
+      }
+    }
+  };
 }
